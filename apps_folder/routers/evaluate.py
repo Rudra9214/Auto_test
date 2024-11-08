a0743c2f01e83c1feb30ea9c1ab5a200350197ef
Rudraraj Thakar <thakarr9214@gmail.com>
2024-11-08 10:11:12 +0530

    Create evaluate.py

---
+++ apps_folder/routers/evaluate.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, HTTPException, Depends
+from apps_folder import models
+from ..services.ai_service import EvaluateService
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+
+router = APIRouter(prefix="/testcase", tags=["evaluate"])
+
+
+@router.post("/", response_model=None)
+async def evaluate_testcase(testcase: models.TestCasesInput):
+    """
+    Endpoint to evaluate test cases using the EvaluateService.
+    """
+    try:
+        # Initialize the EvaluateService with payload and request
+        service = EvaluateService(testcase.model_dump())
+
+        # Generate the test case prompt using the service and request
+        result = await service.start_process()
+
+        return result
+
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Internal server error: {e}")
